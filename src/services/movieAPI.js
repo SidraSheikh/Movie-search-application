94286b8cefecdaf7fb22e4a54ed3f18df42dc000
Sidra <sidrasheikh29303@gmail.com>
2025-02-07 21:13:10 +0500

    Fixed HTTPS API issue for OMDB

--- src/services/movieAPI.js
+++ src/services/movieAPI.js
@@ -1,16 +1,11 @@
 import axios from "axios";
 
-const API_KEY = "f36a2f6a"; 
-const BASE_URL = "http://www.omdbapi.com/";
+const API_KEY = "f36a2f6a";
+const BASE_URL = "https://www.omdbapi.com/"; // ✅ Changed to HTTPS
 
 export const searchMovies = async (query) => {
   try {
-    const response = await axios.get(BASE_URL, {
-      params: {
-        apikey: API_KEY,
-        s: query
-      }
-    });
+    const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}&s=${query}`);
     return response.data.Search || [];
   } catch (error) {
     console.error("Error fetching movies:", error);
